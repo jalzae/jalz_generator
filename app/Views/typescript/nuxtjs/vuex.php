1a73a881bcee6c549c2f61410a1d7c5c48a8cfc8
jalzae <jalzae@gmail.com>
2022-09-08 15:09:53 +0700

    Fix vuex Ts nuxtjs Ts

--- app/Views/typescript/nuxtjs/vuex.php
+++ app/Views/typescript/nuxtjs/vuex.php
@@ -1,26 +1,36 @@
 import { GetterTree, ActionTree, MutationTree } from 'vuex'
 
-export const state = () => ({
-<?= $table . 's' ?>: {
+interface <?= ucfirst($table) ?> {
 <?php
+$type1 = '';
 for ($i = 0; $i < $limit; $i++) {
-
   if (strpos($column[$i]['Type'], 'int') !== false) {
     $type = 'number';
   } else {
     $type = 'String';
   }
 
+  if ($i == 0) {
+    $type1 = $type;
+  }
+
   echo $column[$i]['Field'] . ':' . $type . ',';
 }
-?> }[] =[]
-})
+?>
+}
 
-export type RootState = ReturnType<typeof state>
+const <?= $table ?>: Array<<?= ucfirst($table) ?>> = [
+  ];
 
-  export const getters: GetterTree<RootState, RootState> = {
-    <?php
-    echo '
+  export const state = () => ({
+  <?= $table . 's' ?>: <?= $table ?>
+  })
+
+  export type RootState = ReturnType<typeof state>
+
+    export const getters: GetterTree<RootState, RootState> = {
+      <?php
+      echo '
     get' . $table . '(state) {
     return state.' . $table . 's
     },
@@ -32,23 +42,35 @@ export type RootState = ReturnType<typeof state>
     })
     },
 
-    findindex' . $table . ': (state, getters) => (id) => {
+    findindex' . $table . ': (state, getters) => (id:' . $type1 . ') => {
     return state.' . $table . 's.findIndex(' . $table . ' => ' . $table . '.' . $column[0]['Field'] . ' == id)
     },
 
-    findcontains' . $table . ': (state, getters) => (id) => {
-    return state.' . $table . 's.filter(' . $table . ' => ' . $table . '.' . $column[0]['Field'] . '.toLowerCase().includes(this.id))
-    },
+    findcontains' . $table . ': (state, getters) => (id:any) => {
+    return state.' . $table . 's.filter(e  => ';
+      for ($i = 0; $i < $limit; $i++) {
+        if (strpos($column[$i]['Type'], 'int') !== false) {
+          echo   'e.' . $column[$i]['Field'] . '=== id';
+        } else {
+          echo   'e.' . $column[$i]['Field'] . '.toLowerCase().includes(id)';
+        }
+        if ($i + 1 != $limit) {
+          echo "||";
+        } else {
+          echo ")";
+        }
+      }
+      echo '},
 
-    detail' . $table . ': (state, getters) => (id) => {
+    detail' . $table . ': (state, getters) => (id:' . $type1 . ') => {
     return state.' . $table . 's.find(' . $table . ' => ' . $table . '.' . $column[0]['Field'] . ' == id)
     },';
-    ?>
-    }
+      ?>
+      }
 
-    export const mutations: MutationTree<RootState> = {
-      <?php
-      echo '
+      export const mutations: MutationTree<RootState> = {
+        <?php
+        echo '
       get' . $table . '(state, data) {
       state.' . $table . 's=data;
       },
@@ -58,22 +80,22 @@ export type RootState = ReturnType<typeof state>
       },
       update' . $table . '(state, data) {
       var ' . $table . ' = state.' . $table . 's.find(o => o.' . $column[0]['Field'] . ' == data.' . $column[0]['Field'] . ')
-      var index = state.' . $table . 's.indexOf(' . $table . ');';
+      var index = state.' . $table . 's.indexOf(' . $table . ' as  ' . ucfirst($table) . ');';
 
 
-      for ($i = 1; $i < $limit; $i++) {
-        echo 'state.' . $table . 's[index].' . $column[$i]['Field'] . ' = data.' . $column[$i]['Field'] . ';';
-      }
-      echo '
+        for ($i = 1; $i < $limit; $i++) {
+          echo 'state.' . $table . 's[index].' . $column[$i]['Field'] . ' = data.' . $column[$i]['Field'] . ';';
+        }
+        echo '
         },
         delete' . $table . '(state, value) {
             var ' . $table . ' = state.' . $table . 's.find(o => o.' . $column[0]['Field'] . ' == value)
-            state.' . $table . 's.splice(state.' . $table . 's.indexOf(' . $table . '), 1)
+            state.' . $table . 's.splice(state.' . $table . 's.indexOf(' . $table . ' as  ' . ucfirst($table) . '), 1)
         }';
 
-      ?>
-      }
-
-      export const actions: ActionTree<RootState, RootState> = {
-
+        ?>
         }
+
+        export const actions: ActionTree<RootState, RootState> = {
+
+          }
